78cb406e10a5a5a90b148467b854788976e90ea8
Maximiliano teodosio <matmatmaf@yahoo.com.ar>
2021-05-10 10:45:34 -0300

    merge Julio ABM Vehiculo

--- frontend/src/app/lista-vehiculo/vehiculo-editar/vehiculo-editar.component.ts
+++ frontend/src/app/lista-vehiculo/vehiculo-editar/vehiculo-editar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Vehiculo } from 'src/app/module/vehiculo.module';
 
 @Component({
   selector: 'app-vehiculo-editar',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehiculo-editar.component.css']
 })
 export class VehiculoEditarComponent implements OnInit {
+  @Input() vehiculoModificado = new Vehiculo();
+  @Output() vehiculoEditEvent = new EventEmitter();
+  submitted = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  modifyVehiculo(formulario: NgForm) {
+    if(formulario.valid) 
+    {
+      this.submitted = true;
+      this.vehiculoEditEvent.emit();
+    }
   }
 
 }
