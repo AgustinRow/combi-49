225fa99afe579be8922efbfefb68571583bd5f44
AgustinRow <agus.diaz.11@gmail.com>
2021-06-02 11:56:41 -0300

    feat: se agrego ruta para obtener perfil de usuario

--- app/routes/usuario.js
+++ app/routes/usuario.js
@@ -4,15 +4,21 @@ const Usuario = require("../controller/usuario");
 const verify = require("../controller/verifyToken");
 const auth = require("../controller/auth");
 
-router.get("/lista_choferes", Usuario.getAllDrivers);
-router.get("/lista_pasajeros", Usuario.listPassengers)
+//rutas pasajeros
+router.get("/perfil/(:id)", Usuario.profile);
 
-router.post("/alta_chofer", Usuario.register);
-router.post("/registrar", Usuario.register);
+//rutas acceso admi/chofer/pasajero
 router.put("/modificar_usuario", Usuario.update);
+router.post("/registrar", Usuario.register);
 router.post("/login", Usuario.login);
-router.get("/buscar/(:id)", Usuario.findUser);
-//TODO:
+
+//ruta admin
+router.get("/lista_pasajeros", Usuario.listPassengers);
+router.get("/lista_choferes", Usuario.getAllDrivers);
+router.post("/alta_chofer", Usuario.register);
 router.delete("/borrar/(:id)", Usuario.remove);
+router.get("/buscar/(:id)", Usuario.findUser);
+
+//TODO:
 
 module.exports = router;
