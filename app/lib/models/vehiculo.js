d8b6f2d76d238130176d1b2478b7069cc9d05a47
AgustinRow <agus.diaz.11@gmail.com>
2021-06-08 10:30:18 -0300

    fix: solucion de asociacion

--- app/lib/models/vehiculo.js
+++ app/lib/models/vehiculo.js
@@ -10,10 +10,10 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Vehiculo.hasMany(models.Viaje, { foreignKey: "ViajeId" });
-      /*Vehiculo.hasOne(models.Usuario, {
+      Vehiculo.hasOne(models.Usuario, {
         as: "Chofer",
         foreignKey: "vehiculoId",
-      });*/
+      });
     }
   }
   Vehiculo.init(
