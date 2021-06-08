e473100871c3032c3e571a6f562a0b3d65fc6da5
AgustinRow <agus.diaz.11@gmail.com>
2021-06-08 10:21:25 -0300

    Delete app directory

--- app/routes/vianda.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const Vianda = require("../controller/vianda");
-//const Estado = require("../controller/estado");
-//const verify = require("../controller/verifyPayment");
-router.post("/alta", Vianda.create);
-router.put("/modificar", Vianda.update);
-
-module.exports = router;
